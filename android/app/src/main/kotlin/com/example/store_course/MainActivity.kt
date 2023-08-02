d98f3cde81934871f840ac9115db25901c6712fc
somaa1 <ismailrabie111@gmail.com>
2023-08-02 15:42:20 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/store_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.store_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/resources/manager_strings.dart
@@ -0,0 +1,4 @@
+class ManagerStrings {
+  static const String appName = 'Shop App';
+  static const String aboutScreen = 'About Screen';
+}
